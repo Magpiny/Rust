f839282850aaec82f809f6694d885aada10c355c
Magpiny <magpinyb@gmail.com>
2021-07-31 20:53:14 +0300

    initial commit

---
+++ helloworld/hello.rs
@@ -0,0 +1,13 @@
+//I feel so rusty today : Systems programming language for the 21st century!
+fn main(){
+	
+	println!("Name: {} Hobby: {} Year: {}", "Samuel Wanjare", "Programmer", 2021 );
+
+	}
+
+
+
+
+
+
+
